961177663e1b0c3ec508388ebadbebecb37518dc
muthuri-dev <muthurikennedy082@gmail.com>
2024-05-07 21:27:01 +0300

    feat(schema):Create dto,types,and entities

--- server/apps/users/src/users.module.ts
+++ server/apps/users/src/users.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: {
+        federation: 2,
+      },
+    }),
+  ],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, ConfigService], // configService - Configuration module for Nest based on the dotenv (to load process environment variables)
 })
 export class UsersModule {}
